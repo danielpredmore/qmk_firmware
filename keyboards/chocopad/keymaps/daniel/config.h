c4abc5c540e2e0cd0f439427ab95431de3dcbc9f
Daniel Predmore <dpredmore@witekio.com>
2019-02-04 11:43:33 -0800

    Add chocopad keymap

---
+++ keyboards/chocopad/keymaps/daniel/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#undef RGBLED_NUM
+#define RGBLED_ANIMATIONS
+#define RGBLED_NUM 15
+#define RGBLED_HUE_STEP 8
+#define RGBLED_SAT_STEP 8
+#define RGBLED_VAL_STEP 8
+
+#endif
